7acdd7b504cc14ca79c91ace2fb1a9b20f74b13b
Rohith <rohithravi.0103@gmail.com>
2026-06-05 18:22:47 +0530

    fixed auth cookies for production

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -28,7 +28,13 @@ async function registerUser(req, res) {
     const token = jwt.sign({
         id: user._id,
     }, process.env.JWT_SECRET)
-    res.cookie('token', token)
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    })
 
     res.status(201).json({
         message: 'User registered successfully',
@@ -64,7 +70,12 @@ async function loginUser(req, res) {
         id: user._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie('token', token)
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    })
 
     res.status(200).json({
         message: 'User logged in successfully',
@@ -77,7 +88,11 @@ async function loginUser(req, res) {
 }
 
 function logoutUser(req, res) {
-    res.clearCookie('token');
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+    });
     res.status(200).json({
         message: 'User logged out successfully'
     });
@@ -111,7 +126,13 @@ async function registerFoodPartner(req, res) {
     const token = jwt.sign({
         id: foodPartner._id,
     }, process.env.JWT_SECRET)
-    res.cookie('token', token)
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    })
 
     res.status(201).json({
         message: 'Food Partner registered successfully',
@@ -150,7 +171,12 @@ async function loginFoodPartner(req, res) {
         id: foodPartner._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie('token', token)
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    })
 
     res.status(200).json({
         message: 'Food Partner logged in successfully',
@@ -163,7 +189,12 @@ async function loginFoodPartner(req, res) {
 }
 
 function logoutFoodPartner(req, res) {
-    res.clearCookie('token');
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+    });
+
     res.status(200).json({
         message: 'Food Partner logged out successfully'
     });
