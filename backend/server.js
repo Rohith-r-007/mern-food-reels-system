a8b01e147db4e6a787129d5a27841f84acec09b7
Rohith <rohithravi.0103@gmail.com>
2026-06-05 18:08:27 +0530

    added url and fixed stuff

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const connectDB = require('./src/db/db.js'); // connecting to datasbase
 
 connectDB();// databse connection
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -6,12 +6,13 @@ const authRoutes = require('./routes/auth.routes.js');
 const foodRoutes = require('./routes/food.routes.js');
 const foodPartnerRoutes = require('./routes/food-partner.routes.js');
 
+
 const cors = require('cors');
 
 const app = express();// creating an instance of express
 
 // middlewares
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', process.env.CLIENT_URL];
 app.use(cors({
     origin: function(origin, callback) {
         if (!origin) return callback(null, true);
